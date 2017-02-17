547b57d908191a187cb1b5137d773f4cc6448692
harshitsinha9000 <harshitsinha9000@gmail.com>
2017-02-17 13:10:39 +0530

    Add files via upload

---
+++ Fourth.java
@@ -0,0 +1,18 @@
+package java_session1;
+
+public class Fourth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i;
+		StringBuilder str= new StringBuilder();
+		
+		for(i=65;i<=90;i++)
+		{
+			
+			System.out.println(i+" = "+(char)i);
+		}
+	}
+
+}
